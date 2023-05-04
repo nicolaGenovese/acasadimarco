af5d374552988bb4d475a00e27bdc4029a6f3330
Nicola Genovese <93256991+nicolaGenovese@users.noreply.github.com>
2023-05-04 10:23:19 +0100

    Initial commit

---
+++ jquery/hideShow.js
@@ -0,0 +1,161 @@
+$(document).ready(function() {
+    // Hide the content initially
+    $('.raciones-content').hide();
+  
+    // Attach click event handler to the toggle button
+    $('.raciones-toggle').click(function() {
+      // Toggle the content display
+      $('.raciones-content').slideToggle();
+    });
+
+    // Hide the content initially
+    $('.raciones-de-marisco-content').hide();
+  
+    // Attach click event handler to the toggle button
+    $('.raciones-de-marisco-toggle').click(function() {
+    // Toggle the content display
+    $('.raciones-de-marisco-content').slideToggle();
+    });
+
+    // Hide the content initially
+    $('.tostadas-content').hide();
+  
+    // Attach click event handler to the toggle button
+    $('.tostadas-toggle').click(function() {
+    // Toggle the content display
+    $('.tostadas-content').slideToggle();
+    });
+
+    // Hide the content initially
+    $('.platos-de-mi-casa-content').hide();
+  
+    // Attach click event handler to the toggle button
+    $('.platos-de-mi-casa-toggle').click(function() {
+    // Toggle the content display
+    $('.platos-de-mi-casa-content').slideToggle();
+    });
+
+       // Hide the content initially
+       $('.hamburguesas-content').hide();
+  
+       // Attach click event handler to the toggle button
+       $('.hamburguesas-toggle').click(function() {
+       // Toggle the content display
+       $('.hamburguesas-content').slideToggle();
+       });
+
+// Hide the content initially
+$('.ensaladas-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.ensaladas-toggle').click(function() {
+// Toggle the content display
+$('.ensaladas-content').slideToggle();
+});
+
+// Hide the content initially
+$('.boccalillos-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.boccalillos-toggle').click(function() {
+// Toggle the content display
+$('.boccalillos-content').slideToggle();
+});
+
+// Hide the content initially
+$('.postre-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.postre-toggle').click(function() {
+// Toggle the content display
+$('.postre-content').slideToggle();
+});
+
+// Hide the content initially
+$('.te-cafe-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.te-cafe-toggle').click(function() {
+// Toggle the content display
+$('.te-cafe-content').slideToggle();
+});
+
+// Hide the content initially
+$('.zumos-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.zumos-toggle').click(function() {
+// Toggle the content display
+$('.zumos-content').slideToggle();
+});
+
+// Hide the content initially
+$('.refrescos-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.refrescos-toggle').click(function() {
+// Toggle the content display
+$('.refrescos-content').slideToggle();
+});
+
+// Hide the content initially
+$('.vinos-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.vinos-toggle').click(function() {
+// Toggle the content display
+$('.vinos-content').slideToggle();
+});
+
+// Hide the content initially
+$('.cervezas-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.cervezas-toggle').click(function() {
+// Toggle the content display
+$('.cervezas-content').slideToggle();
+});
+
+// Hide the content initially
+$('.cocktails-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.cocktails-toggle').click(function() {
+// Toggle the content display
+$('.cocktails-content').slideToggle();
+});
+
+// Hide the content initially
+$('.chupitos-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.chupitos-toggle').click(function() {
+// Toggle the content display
+$('.chupitos-content').slideToggle();
+});
+
+// Hide the content initially
+$('.para-picar-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.para-picar-toggle').click(function() {
+// Toggle the content display
+$('.para-picar-content').slideToggle();
+});
+
+// Hide the content initially
+$('.infantil-content').hide();
+  
+// Attach click event handler to the toggle button
+$('.infantil-toggle').click(function() {
+// Toggle the content display
+$('.infantil-content').slideToggle();
+});
+
+
+
+
+
+
+  });
+
