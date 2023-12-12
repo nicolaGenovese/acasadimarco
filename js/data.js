380f8a11c23f7a690c7185a06c78cecf14ad6337
Nicola Genovese <nicol.genovese@gmail.com>
2023-12-12 10:09:10 +0000

    inizio oggetto menu

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const menu = [
+    {
+      categoria: "RACIONES",
+      piatti: [
+        { descrizione: "ARANCINO CLÁSICO ITALIANO (4 UNIDADES)", prezzo: 6.5 },
+        { descrizione: "FRITTATINA NAPOLETANA (CROQUETA DE PASTA, 4 UNIDADES)", prezzo: 6.5 },
+        { descrizione: "PAPAS BRAVAS", prezzo: 8.9 },
+        { descrizione: "CALAMARES CON SALSA DE ALIOLI", prezzo: 10.5 },
+        { descrizione: "NACHOS CON QUESO, GUACAMOLE, CHILI CON CARNE Y JALAPEÑOS", prezzo: 10.5 },
+        { descrizione: "INVOLTINI DI MELANZANE RIPIENI IN SALSA CASERA", prezzo: 10.5 }
+      ]
+    },
+    {
+      categoria: "Primi Piatti",
+      piatti: [
+        { descrizione: "Sugo di carne con pomodoro", prezzo: 12.99 },
+        { descrizione: "Risotto con funghi porcini", prezzo: 14.99 }
+      ]
+    },
+    // Aggiungi altre categorie e piatti secondo le tue esigenze
+  ];
