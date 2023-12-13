b9f562bd7ca15be5e64cd94e0d1008477551e0d9
Nicola Genovese <nicol.genovese@gmail.com>
2023-12-13 00:02:07 +0000

    prova dati injection

--- js/data.js
+++ js/data.js
@@ -11,11 +11,130 @@ const menu = [
       ]
     },
     {
-      categoria: "Primi Piatti",
+      categoria: "PASTA",
       piatti: [
-        { descrizione: "Sugo di carne con pomodoro", prezzo: 12.99 },
-        { descrizione: "Risotto con funghi porcini", prezzo: 14.99 }
+        { descrizione: "ESPAGUETIS CON SALSA BOLOÑESA", prezzo: 9.0 },
+        { descrizione: "MACARRONES A LA BOSCAIOLA", prezzo: 11.9 },
+        { descrizione: "MACARRONES CON PESTO Y CHERRY TOMATE ASADO", prezzo: 9.9 },
+        { descrizione: "ESPAGUETIS CON SALSA CARBONARA", prezzo: 12.5 }
+      ]
+    },
+    {
+      categoria: "TOSTADAS",
+      piatti: [
+        { descrizione: "AGUACATE CON HUEVO ESCALFADO Y BACON CRUJIENTE", prezzo: 9.0 },
+        { descrizione: "CUATRO QUESOS Y BACON", prezzo: 11.9 }
       ]
     },
     // Aggiungi altre categorie e piatti secondo le tue esigenze
   ];
+
+
+
+  const html = `<div class="col-lg-4 themenu_column">
+  <div class="themenu_col">
+     <div class="themenu_col_title raciones-toggle">RACIONES</div>
+     <div class="dish_list raciones-content">
+        <!-- Dish -->
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">ARANCINO CLÁSICO
+                 ITALIANO (4 UNIDADES)</div>
+              <div class="dish_price">6.5€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+        <!-- Dish -->
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">FRITTATINA NAPOLETANA
+                 (CROQUETA DE PASTA, 4 UNIDADES)</div>
+              <div class="dish_price">6.5€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+        <!-- Dish -->
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">PAPAS BRAVAS </div>
+              <div class="dish_price">8.9€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+        <!-- Dish -->
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">CALAMARES CON SALSA DE
+                 ALIOLI</div>
+              <div class="dish_price">10.5€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+        <!-- Dish -->
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">NACHOS CON QUESO,
+                 GUACAMOLE, CHILI CON CARNE Y JALAPEÑOS</div>
+              <div class="dish_price">10.5€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+        <div class="dish">
+           <div
+              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
+              <div class="dish_title">INVOLTINI DI MELANZANE
+                 RIPIENI IN SALSA CASERA</div>
+              <div class="dish_price">10.5€</div>
+           </div>
+           <!-- <div class="dish_contents">
+                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
+                                   <li>Pork</li>
+                                   <li>Tenderloin</li>
+                                   <li>Green Pepper</li>
+                                </ul>
+                                </div> -->
+           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
+        </div>
+     </div>
+  </div>
+</div>`
