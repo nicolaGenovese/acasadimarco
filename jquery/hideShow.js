e55aff094d077451073e4068704dcff682d614e8
Nicola Genovese <nicol.genovese@gmail.com>
2023-12-13 13:37:38 +0000

    oggetto dati funzionante

--- jquery/hideShow.js
+++ jquery/hideShow.js
@@ -1,188 +1,188 @@
-$(document).ready(function() {
-   // Hide the content initially
-   $('.oferta-especial1-content').hide();
-  
-   // Attach click event handler to the toggle button
-   $('.oferta-especial1-toggle').click(function() {
-     // Toggle the content display
-     $('.oferta-especial1-content').slideToggle();
-   });
+$(document).ready(function () {
+  // Hide the content initially
+  $('.oferta-especial1-content').hide();
 
-   // Hide the content initially
-   $('.oferta-especial2-content').hide();
-  
-   // Attach click event handler to the toggle button
-   $('.oferta-especial2-toggle').click(function() {
-     // Toggle the content display
-     $('.oferta-especial2-content').slideToggle();
-   });
+  // Attach click event handler to the toggle button
+  $('.oferta-especial1-toggle').click(function () {
+    // Toggle the content display
+    $('.oferta-especial1-content').slideToggle();
+  });
 
-   // Hide the content initially
-   $('.oferta-especial3-content').hide();
-  
-   // Attach click event handler to the toggle button
-   $('.oferta-especial3-toggle').click(function() {
-     // Toggle the content display
-     $('.oferta-especial3-content').slideToggle();
-   });
+  // Hide the content initially
+  $('.oferta-especial2-content').hide();
 
-    // Hide the content initially
-    $('.raciones-content').hide();
-  
-    // Attach click event handler to the toggle button
-    $('.raciones-toggle').click(function() {
-      // Toggle the content display
-      $('.raciones-content').slideToggle();
-    });
+  // Attach click event handler to the toggle button
+  $('.oferta-especial2-toggle').click(function () {
+    // Toggle the content display
+    $('.oferta-especial2-content').slideToggle();
+  });
 
-    // Hide the content initially
-    $('.pasta-content').hide();
-  
-    // Attach click event handler to the toggle button
-    $('.pasta-toggle').click(function() {
+  // Hide the content initially
+  $('.oferta-especial3-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.oferta-especial3-toggle').click(function () {
+    // Toggle the content display
+    $('.oferta-especial3-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.raciones-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.raciones-toggle').click(function () {
+    // Toggle the content display
+    $('.raciones-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.pasta-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.pasta-toggle').click(function () {
     // Toggle the content display
     $('.pasta-content').slideToggle();
-    });
+  });
 
-    // Hide the content initially
-    $('.tostadas-content').hide();
-  
-    // Attach click event handler to the toggle button
-    $('.tostadas-toggle').click(function() {
+  // Hide the content initially
+  $('.tostadas-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.tostadas-toggle').click(function () {
     // Toggle the content display
     $('.tostadas-content').slideToggle();
-    });
+  });
 
-    // Hide the content initially
-    $('.platos-de-mi-casa-content').hide();
-  
-    // Attach click event handler to the toggle button
-    $('.platos-de-mi-casa-toggle').click(function() {
+  // Hide the content initially
+  $('.platos-de-mi-casa-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.platos-de-mi-casa-toggle').click(function () {
     // Toggle the content display
     $('.platos-de-mi-casa-content').slideToggle();
-    });
-
-       // Hide the content initially
-       $('.hamburguesas-content').hide();
-  
-       // Attach click event handler to the toggle button
-       $('.hamburguesas-toggle').click(function() {
-       // Toggle the content display
-       $('.hamburguesas-content').slideToggle();
-       });
-
-// Hide the content initially
-$('.risotto-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.risotto-toggle').click(function() {
-// Toggle the content display
-$('.risotto-content').slideToggle();
-});
-
-// Hide the content initially
-$('.boccadillos-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.boccadillos-toggle').click(function() {
-// Toggle the content display
-$('.boccadillos-content').slideToggle();
-});
-
-// Hide the content initially
-$('.postre-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.postre-toggle').click(function() {
-// Toggle the content display
-$('.postre-content').slideToggle();
-});
-
-// Hide the content initially
-$('.te-cafe-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.te-cafe-toggle').click(function() {
-// Toggle the content display
-$('.te-cafe-content').slideToggle();
-});
-
-// Hide the content initially
-$('.zumos-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.zumos-toggle').click(function() {
-// Toggle the content display
-$('.zumos-content').slideToggle();
-});
-
-// Hide the content initially
-$('.refrescos-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.refrescos-toggle').click(function() {
-// Toggle the content display
-$('.refrescos-content').slideToggle();
-});
-
-// Hide the content initially
-$('.vinos-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.vinos-toggle').click(function() {
-// Toggle the content display
-$('.vinos-content').slideToggle();
-});
-
-// Hide the content initially
-$('.cervezas-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.cervezas-toggle').click(function() {
-// Toggle the content display
-$('.cervezas-content').slideToggle();
-});
-
-// Hide the content initially
-$('.cocktails-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.cocktails-toggle').click(function() {
-// Toggle the content display
-$('.cocktails-content').slideToggle();
-});
-
-// Hide the content initially
-$('.chupitos-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.chupitos-toggle').click(function() {
-// Toggle the content display
-$('.chupitos-content').slideToggle();
-});
-
-// Hide the content initially
-$('.para-picar-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.para-picar-toggle').click(function() {
-// Toggle the content display
-$('.para-picar-content').slideToggle();
-});
-
-// Hide the content initially
-$('.infantil-content').hide();
-  
-// Attach click event handler to the toggle button
-$('.infantil-toggle').click(function() {
-// Toggle the content display
-$('.infantil-content').slideToggle();
-});
-
-
-
-
-
-
   });
+
+  // Hide the content initially
+  $('.hamburguesas-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.hamburguesas-toggle').click(function () {
+    // Toggle the content display
+    $('.hamburguesas-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.risoto-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.risoto-toggle').click(function () {
+    // Toggle the content display
+    $('.risoto-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.bocadillos-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.bocadillos-toggle').click(function () {
+    // Toggle the content display
+    $('.bocadillos-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.postre-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.postre-toggle').click(function () {
+    // Toggle the content display
+    $('.postre-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.te-cafe-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.te-cafe-toggle').click(function () {
+    // Toggle the content display
+    $('.te-cafe-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.zumos-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.zumos-toggle').click(function () {
+    // Toggle the content display
+    $('.zumos-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.refrescos-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.refrescos-toggle').click(function () {
+    // Toggle the content display
+    $('.refrescos-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.vinos-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.vinos-toggle').click(function () {
+    // Toggle the content display
+    $('.vinos-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.cervezas-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.cervezas-toggle').click(function () {
+    // Toggle the content display
+    $('.cervezas-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.cocktails-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.cocktails-toggle').click(function () {
+    // Toggle the content display
+    $('.cocktails-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.chupitos-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.chupitos-toggle').click(function () {
+    // Toggle the content display
+    $('.chupitos-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.para-picar-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.para-picar-toggle').click(function () {
+    // Toggle the content display
+    $('.para-picar-content').slideToggle();
+  });
+
+  // Hide the content initially
+  $('.infantil-content').hide();
+
+  // Attach click event handler to the toggle button
+  $('.infantil-toggle').click(function () {
+    // Toggle the content display
+    $('.infantil-content').slideToggle();
+  });
+
+
+
+
+
+
+});
 
--- js/data.js
+++ js/data.js
@@ -2,28 +2,42 @@ const menu = [
     {
       categoria: "RACIONES",
       piatti: [
-        { descrizione: "ARANCINO CLÁSICO ITALIANO (4 UNIDADES)", prezzo: 6.5 },
-        { descrizione: "FRITTATINA NAPOLETANA (CROQUETA DE PASTA, 4 UNIDADES)", prezzo: 6.5 },
-        { descrizione: "PAPAS BRAVAS", prezzo: 8.9 },
-        { descrizione: "CALAMARES CON SALSA DE ALIOLI", prezzo: 10.5 },
-        { descrizione: "NACHOS CON QUESO, GUACAMOLE, CHILI CON CARNE Y JALAPEÑOS", prezzo: 10.5 },
-        { descrizione: "INVOLTINI DI MELANZANE RIPIENI IN SALSA CASERA", prezzo: 10.5 }
+        { nomePiatto: "ARANCINO CLÁSICO ITALIANO (4 UNIDADES)", prezzo: 6.5 },
+        { nomePiatto: "FRITTATINA NAPOLETANA (CROQUETA DE PASTA, 4 UNIDADES)", prezzo: 6.5 },
+        { nomePiatto: "PAPAS BRAVAS", prezzo: 8.9 },
+        { nomePiatto: "CALAMARES CON SALSA DE ALIOLI", prezzo: 10.5 },
+        { nomePiatto: "NACHOS CON QUESO, GUACAMOLE, CHILI CON CARNE Y JALAPEÑOS", prezzo: 10.5 },
+        { nomePiatto: "INVOLTINI DI MELANZANE RIPIENI IN SALSA CASERA", prezzo: 10.5 }
       ]
     },
     {
       categoria: "PASTA",
       piatti: [
-        { descrizione: "ESPAGUETIS CON SALSA BOLOÑESA", prezzo: 9.0 },
-        { descrizione: "MACARRONES A LA BOSCAIOLA", prezzo: 11.9 },
-        { descrizione: "MACARRONES CON PESTO Y CHERRY TOMATE ASADO", prezzo: 9.9 },
-        { descrizione: "ESPAGUETIS CON SALSA CARBONARA", prezzo: 12.5 }
+        { nomePiatto: "ESPAGUETIS CON SALSA BOLOÑESA", prezzo: 9.0 },
+        { nomePiatto: "MACARRONES A LA BOSCAIOLA", prezzo: 11.9 },
+        { nomePiatto: "MACARRONES CON PESTO Y CHERRY TOMATE ASADO", prezzo: 9.9 },
+        { nomePiatto: "ESPAGUETIS CON SALSA CARBONARA", prezzo: 12.5 }
       ]
     },
     {
       categoria: "TOSTADAS",
       piatti: [
-        { descrizione: "AGUACATE CON HUEVO ESCALFADO Y BACON CRUJIENTE", prezzo: 9.0 },
-        { descrizione: "CUATRO QUESOS Y BACON", prezzo: 11.9 }
+        { nomePiatto: "AGUACATE CON HUEVO ESCALFADO Y BACON CRUJIENTE", prezzo: 9.0 },
+        { nomePiatto: "CUATRO QUESOS Y BACON", prezzo: 11.9 }
+      ]
+    },
+    {
+      categoria: "PLATOS DE MI CASA",
+      piatti: [
+         { nomePiatto: "HUEVOS ROTOS CON JAMÓN", prezzo: 9.0 },
+         { nomePiatto: "CUATRO QUESOS Y BACON", prezzo: 11.9 },
+         { nomePiatto: "AUTÉNTICA BERENJENA DE PARMESANO", prezzo: 11.5 },
+         { nomePiatto: "LASAÑA TRADICIONAL ITALIANA", prezzo: 11.5 },
+         { nomePiatto: "RÚCULA, TOMATE CHERRY ASADO, PARMESANO Y BALSÁMICO", prezzo: 6.9 },
+         { nomePiatto: "BURRATA CON SALSA ITALIANA", prezzo: 10.9 },
+         { nomePiatto: "CAPONATA (GUISO CLÁSICO DE LA COCINA SICILIANA)", prezzo: 8.5 },
+         { nomePiatto: "SALTIMBOCCA DI POLLO ALLA ROMANA", prezzo: 11.9 },
+         { nomePiatto: "PANCETTA ITALIANA AL FORNO CON GUARNICIÓN", prezzo: 14.9 }
       ]
     },
     // Aggiungi altre categorie e piatti secondo le tue esigenze
@@ -31,110 +45,4 @@ const menu = [
 
 
 
-  const html = `<div class="col-lg-4 themenu_column">
-  <div class="themenu_col">
-     <div class="themenu_col_title raciones-toggle">RACIONES</div>
-     <div class="dish_list raciones-content">
-        <!-- Dish -->
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">ARANCINO CLÁSICO
-                 ITALIANO (4 UNIDADES)</div>
-              <div class="dish_price">6.5€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-        <!-- Dish -->
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">FRITTATINA NAPOLETANA
-                 (CROQUETA DE PASTA, 4 UNIDADES)</div>
-              <div class="dish_price">6.5€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-        <!-- Dish -->
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">PAPAS BRAVAS </div>
-              <div class="dish_price">8.9€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-        <!-- Dish -->
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">CALAMARES CON SALSA DE
-                 ALIOLI</div>
-              <div class="dish_price">10.5€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-        <!-- Dish -->
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">NACHOS CON QUESO,
-                 GUACAMOLE, CHILI CON CARNE Y JALAPEÑOS</div>
-              <div class="dish_price">10.5€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-        <div class="dish">
-           <div
-              class="dish_title_container d-flex flex-xl-row flex-column align-items-start justify-content-start">
-              <div class="dish_title">INVOLTINI DI MELANZANE
-                 RIPIENI IN SALSA CASERA</div>
-              <div class="dish_price">10.5€</div>
-           </div>
-           <!-- <div class="dish_contents">
-                                <ul class="d-flex flex-row align-items-start justify-content-start flex-wrap">
-                                   <li>Pork</li>
-                                   <li>Tenderloin</li>
-                                   <li>Green Pepper</li>
-                                </ul>
-                                </div> -->
-           <!-- <div class="dish_order"><a href="#">Order Now</a></div> -->
-        </div>
-     </div>
-  </div>
-</div>`
+ 
